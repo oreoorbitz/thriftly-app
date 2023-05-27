cee4254cdf0ea6ef686df75880bf687ace65142a
jagdeep-sg <129896886+jagdeep-sg@users.noreply.github.com>
2023-05-27 14:49:29 +0530

    Update ShopifyController.php

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -8,130 +8,132 @@ use Illuminate\Support\Facades\Http;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use App\Events\PodcastProcessed;
+use DB;
 
 class ShopifyController extends Controller
 {
     protected $shopifyService;
 
-    public function __construct(ShopifyService $shopifyService)
-    {
+    public function __construct(ShopifyService $shopifyService){
         $this->shopifyService = $shopifyService;
     }
 
-    public function countProducts()
-    {
+    public function countProducts(Request $request){
         $products = $this->shopifyService->countProducts();
         return $products;
     }
+    
+    //--Get watch list.
+    public function getWatchlist(Request $request){
+        return true;
+    }
 
-    public function getProducts()
-    {
-        dd('For developer only.');
-        set_time_limit(1000);
-        $count = $this->countProducts();
-        $total_page = ceil($count['count']/50);
-        $page = 1;
-        $since_id = 0;
-        while($page <= $total_page){
-            $products = $this->shopifyService->getProducts($since_id);
-            foreach ($products['products'] as $value) {
-                //--Comment this line for production
-                // if($value['id'] != '8362488398143'){
-                //     continue;
-                // }
-                foreach ($value['variants'] as $variant) {
-                    if($variant['inventory_quantity'] <= 0){
-                        continue;
-                    }
-                    $price = floatval($variant['price']);
-                    $compare_at_price = floatval($variant['compare_at_price']);
-                    $percentage = ($price / $compare_at_price) * 100;
-
-                    $round_count = 1;
+    //--Add watch list.
+    public function addWatchlist(Request $request){
+        $input = $request->input();
+        
+        $product = DB::table("watchlist")->where('product_id', (int)$input['product_id'])->first();
+        if($product){
+            $customer = json_decode($product->customer, true);
+            if(isset($customer[$input['email']])){
+                $response = ['status' => false, 'content' => 'customer already in watchlist.'];
+            }else{
+                $customer[$input['email']] = $input['name'];
+                $data['customer'] = json_encode($customer);
+                $data['watch_count'] = $product->watch_count + 1;
+                DB::table("watchlist")->where('product_id', (int)$input['product_id'])->update($data);
+                
+                $metafields = $this->shopifyService->getMetafield($input['product_id']);
+                if(!isset($metafields['metafields'])){
+                    $response = ['status' => false, 'watch_count' => $data['watch_count'], 'content' => 'customer added in watchlist. Metafield not set.'];
+                }else{
                     $metafield_id = null;
-                    $metafields = $this->shopifyService->getMetafield($variant['id']);
                     foreach ($metafields['metafields'] as $metafield) {
-                        if($metafield['key'] == 'round_count'){
-                            if($metafield['value'] > 0){
-                                $round_count = $metafield['value'] + 1;
-                            }
+                        if($metafield['key'] == 'watch_count'){
                             $metafield_id = $metafield['id'];
                         }
                     }
-                    if($round_count > 16){
-                        continue;
-                    }
-                    if($percentage == 10 || $percentage == 100){
-                        $percentage = 90;
-                    }
-                    
-                    $final_price = ($compare_at_price * ($percentage - 10)) / 100;
                     $variantData = array(
-                        'id' => $variant['id'],
-                        'price' => $final_price,
+                        'id' => $input['product_id'],
                         'metafields' => array(
                             array(
                                 "id" => $metafield_id,
-                                "key" => "round_count",
-                                "value" => $round_count,
+                                "key" => "watch_count",
+                                "value" => $data['watch_count'],
                                 "type" => "number_integer",
                                 "namespace" => "custom"
                             )
                         )
                     );
-                    $response = $this->shopifyService->updateProductVariant($variant['id'], $variantData);
-                    // $this->mailSend();
-                    if($round_count == 9 || $round_count >= 16){
-                        // event(new PodcastProcessed()); //--Remove this product watchlist.
+                    $this->shopifyService->updateProductVariant($input['product_id'], $variantData);
+                    $response = ['status' => true, 'watch_count' => $data['watch_count'], 'content' => 'customer added in watchlist.'];
+                }
+            }
+        }else{
+            $data['product_id'] = $input['product_id'];
+            $data['customer'] = json_encode([$input['email'] => $input['name']]);
+            $data['watch_count'] = 1;
+            DB::table("watchlist")->insert($data);
+
+            $metafields = $this->shopifyService->getMetafield($input['product_id']);
+            if(!isset($metafields['metafields'])){
+                $response = ['status' => false, 'watch_count' => $data['watch_count'], 'content' => 'customer added in watchlist with new product. Metafield not set.'];
+            }else{
+                $metafield_id = null;
+                foreach ($metafields['metafields'] as $metafield) {
+                    if($metafield['key'] == 'watch_count'){
+                        $metafield_id = $metafield['id'];
                     }
                 }
-                
+                $variantData = array(
+                    'id' => $input['product_id'],
+                    'metafields' => array(
+                        array(
+                            "id" => $metafield_id,
+                            "key" => "watch_count",
+                            "value" => $data['watch_count'],
+                            "type" => "number_integer",
+                            "namespace" => "custom"
+                        )
+                    )
+                );
+                $this->shopifyService->updateProductVariant($input['product_id'], $variantData);
+                $response = ['status' => true, 'watch_count' => $data['watch_count'], 'content' => 'customer added in watchlist with new product.'];
             }
-            $since_id = end($products['products'])['id'];
-            $page++;
+            
         }
-        return true;
+        return json_encode($response);
     }
-    
-    //--Get watch list.
-    public function getWatchlist(){}
-
-    //--Add watch list.
-    public function addWatchlist(){}
 
     //--Remove watch list.
-    public function clearWatchlist(){
-		$data = [
-			"product_id"=> 8354369601855,
-			"customer_id"=> 6967658217791,
-			"email"=> "jagdeep.singh109155@gmail.com",
-		];
-		// return $response->json();
-    }
-
-    //--Send mail to customer.
-    private function mailSend(){
-        $mail = new PHPMailer();
-
-        // Email server settings
-        $mail->SMTPDebug = 0;
-        $mail->isSMTP();
-        $mail->Host = env('MAIL_HOST');
-        $mail->SMTPAuth = true;
-        $mail->Username = env('MAIL_USERNAME');
-        $mail->Password = env('MAIL_PASSWORD');
-        $mail->SMTPSecure = env('MAIL_ENCRYPTION');
-        $mail->Port = env('MAIL_PORT');
-        $mail->addAddress('govindersingh0595@gmail.com');
-        $mail->Subject = "Product discount increase";
-        $mail->isHTML(true);
-        $mail->Body = '<h2>Check now!</h2>';  
-        
-        // Send email  
-        if(!$mail->send()){  
-            echo 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo;  
-        }else{  
-            echo 'Message has been sent.';  
+    public function clearWatchlist($id){
+        $metafields = $this->shopifyService->getMetafield($id);
+        if(!isset($metafields['metafields'])){
+            $response = ['status' => false, 'content' => 'Wishlist not clear.'];
+        }else{
+            DB::table("watchlist")->where('product_id', (int)$id)->delete();
+            $metafield_id = null;
+            foreach ($metafields['metafields'] as $metafield) {
+                if($metafield['key'] == 'watch_count'){
+                    $metafield_id = $metafield['id'];
+                }
+            }
+            $variantData = array(
+                'id' => $id,
+                'metafields' => array(
+                    array(
+                        "id" => $metafield_id,
+                        "key" => "watch_count",
+                        "value" => 0,
+                        "type" => "number_integer",
+                        "namespace" => "custom"
+                    )
+                )
+            );
+            $this->shopifyService->updateProductVariant($id, $variantData);
+            $response = ['status' => true, 'content' => 'Wishlist clear.'];
         }
+        return json_encode($response);
     }
+
 }
