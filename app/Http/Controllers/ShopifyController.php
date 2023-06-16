6cc34999d406cc229908cdb5cc648eb284b529ae
jagdeep-sg <129896886+jagdeep-sg@users.noreply.github.com>
2023-06-16 23:41:53 +0530

    Update ShopifyController.php

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -9,6 +9,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use App\Events\PodcastProcessed;
 use DB;
+use Illuminate\Support\Facades\Log;
 
 class ShopifyController extends Controller
 {
@@ -112,18 +113,29 @@ class ShopifyController extends Controller
             $response = ['status' => false, 'content' => 'Wishlist not clear.'];
         }else{
             DB::table("watchlist")->where('product_id', (int)$id)->delete();
-            $metafield_id = null;
+            $watch_count_id = null;
+            $round_count_id = null;
             foreach ($metafields['metafields'] as $metafield) {
                 if($metafield['key'] == 'watch_count'){
-                    $metafield_id = $metafield['id'];
+                    $watch_count_id = $metafield['id'];
+                }
+                if($metafield['key'] == 'round_count'){
+                    $round_count_id = $metafield['id'];
                 }
             }
             $variantData = array(
                 'id' => $id,
                 'metafields' => array(
                     array(
-                        "id" => $metafield_id,
+                        "id" => $watch_count_id,
                         "key" => "watch_count",
+                        "value" => 0,
+                        "type" => "number_integer",
+                        "namespace" => "custom"
+                    ),
+                    array(
+                        "id" => $round_count_id,
+                        "key" => "round_count",
                         "value" => 0,
                         "type" => "number_integer",
                         "namespace" => "custom"
@@ -133,7 +145,61 @@ class ShopifyController extends Controller
             $this->shopifyService->updateProductVariant($id, $variantData);
             $response = ['status' => true, 'content' => 'Wishlist clear.'];
         }
-        return json_encode($response);
+        return $response;
+    }
+
+    public function orderHook(Request $request){
+        $data = file_get_contents('php://input');
+        $order= json_decode($data, true);
+        foreach ($order['line_items'] as $key => $value) {
+
+            $product = $this->shopifyService->getOneProducts($value['product_id']);
+            $collect_new_arrival = $this->shopifyService->getProducts($value['product_id']);
+
+            $this->mailSend($value["variant_id"], $product['product'], $collect_new_arrival['products']);
+        }
+        return;
+    }
+
+    //--Send mail to customer.
+    private function mailSend($id, $product, $collect_new_arrival)
+    {
+        $product_db = DB::table("watchlist")->where('product_id', $id)->first();
+        if(!$product_db){
+            return;
+        }
+        $customers = json_decode($product_db->customer, true);
+        // Log::info($customers);
+        if($customers){
+            foreach ($customers as $email => $name) {
+                //--Email server settings
+                $mail = new PHPMailer(true);
+                $mail->SMTPDebug = 0;
+                $mail->isSMTP();
+                $mail->Host = env('MAIL_HOST');
+                $mail->SMTPAuth = true;
+                $mail->Username = env('MAIL_USERNAME');
+                $mail->Password = env('MAIL_PASSWORD');
+                $mail->SMTPSecure = env('MAIL_ENCRYPTION');
+                $mail->Port = env('MAIL_PORT');
+                $mail->From = env('MAIL_FROM_ADDRESS');
+                $mail->addAddress($email);
+                $mail->Subject = "Thriflty update";
+                $mail->isHTML(true);
+
+                $mail->Body = view('emails.sold_out', ['data' => $product, 'customer_name' => $name, 'new_arraival' => $collect_new_arrival])->render();
+                
+                try {
+                    $mail->send();
+                    // Log::info("sent");
+                } catch (Exception $e) {
+                    Log::error($e);
+                }
+                sleep(1);
+            }
+            $this->clearWatchlist($id);
+        }
+        return true;
     }
 
 }
